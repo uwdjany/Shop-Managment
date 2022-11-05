073732df16db2d7851f170517f55cb793005a322
uwdjany <djanatiuwase@gmail.com>
2022-11-05 21:51:27 +0200

    some changes added

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,42 +1,14 @@
-import UserServices from "../services/userServices";
-import Response from "../utils/response";
-class UserController{
-//create user controller
-    static async createUser(req, res) {
-        const user = await UserServices.createUser(req);
-        if (!user) {
-          return Response.errorMessage(res, "Not Found", 400);
-        }
-    
-        return Response.successMessage(res, "Successful Created", user, 200);
-      }
+import UserModel from "../models/user";
+import {
+  create,
+  getAll,
+  getOneById,
+  updateOneById,
+  deleteOneById,
+} from "./globalController";
 
-      static async getOneById(req, res) {
-        const user = await UserServices.getOneById(req);
-        if (!user) {
-          return Response.errorMessage(res, "Not Found", 400);
-        }
-    
-        return Response.successMessage(res, "Successful Created", user, 200);
-      }
-
-      static async deleteUser(req, res) {
-        const user = await UserServices.getOneById(req);
-        if (!user) {
-          return Response.errorMessage(res, "Not Found", 400);
-        }
-    
-        return Response.successMessage(res, "Successful Created", user, 200);
-      }
-      static async updateUser(req, res) {
-        const user = await UserServices.getOneById(req);
-        if (!user) {
-          return Response.errorMessage(res, "Not Found", 400);
-        }
-    
-        return Response.successMessage(res, "Successful Created", user, 200);
-      }
-
-}
-
-export default UserController;
+export const createController = create(UserModel);
+export const getAllController = getAll(UserModel);
+export const getOneController = getOneById(UserModel);
+export const updateOneController = updateOneById(UserModel);
+export const deleteOneController = deleteOneById(UserModel);
